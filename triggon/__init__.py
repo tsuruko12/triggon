b602f43bcb3f2f5efc7d30d358ddae063a011ca0
Tsuruko <tsuruko-12@outlook.com>
2025-07-08 07:02:50 +0900

    Bump version to 0.1.0b1

--- triggon/__init__.py
+++ triggon/__init__.py
@@ -2,4 +2,6 @@ from .triggon import Triggon
 from .trig_func import TrigFunc
 from ._exceptions import InvalidArgumentError
 
+__version__ = "0.1.0b1"
+
 __all__ = ["Triggon", "TrigFunc", "InvalidArgumentError"]
