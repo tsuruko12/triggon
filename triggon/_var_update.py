88e58841daacf8203bdcf4a3ff6ca53ca12dc998
Tsuruko <tsuruko-12@outlook.com>
2025-07-13 20:00:18 +0900

    feat: add function to find original variable value for revert()

--- triggon/_var_update.py
+++ triggon/_var_update.py
@@ -4,16 +4,22 @@ from typing import Any
 from ._exceptions import SYMBOL
 
 
-def _store_org_value(
-    self, label: str, index: int, org_value: Any,
-) -> None:
+def _store_org_value(self, label: str, index: int, org_value: Any) -> None:
+    target_index = self._org_value[label][index] 
+
     if isinstance(org_value, (list, tuple)):
-      self._org_value[label][index] = []  
+      if target_index is None and not isinstance(target_index, list):
+          self._org_value[label][index] = []
+      elif target_index is not None and not isinstance(target_index, list):
+          self._org_value[label][index] = [target_index]
 
       for v in org_value:
           self._org_value[label][index].append(v)
     else:
-      self._org_value[label][index] = org_value
+      if target_index is None:
+          self._org_value[label][index] = org_value
+      else:
+       self._org_value[label][index].append(org_value)
 
 def _update_var_value(
     self, 
@@ -59,14 +65,21 @@ def _check_exist_label(self, label: str) -> None:
     except KeyError:
         raise KeyError(f"`{label}` has not been set.")
 
-def _check_label_flag(self, label: str) -> None:
+def _check_label_flag(self, label: str, cond: str | None) -> None:
     name = label.lstrip(SYMBOL)
     self._check_exist_label(name)
 
     if self._disable_label[name] or self._trigger_flag[name]:
         return
     
-    self._trigger_flag[name] = True
+    if cond is not None:
+        self._get_target_frame("set_trigger")
+
+        if self._ensure_safe_cond(cond):
+            self._trigger_flag[name] = True
+    else:
+        self._trigger_flag[name] = True
+    
     self._label_has_var(name, "set_trigger", False)
 
     if self.debug:
@@ -103,6 +116,7 @@ def _label_has_var(
             self._update_var_value(arg, update_value[i])     
 
 def _is_new_var(self, label: str, index: int, value: Any) -> bool:
+    # The new value to be set
     if self._trigger_flag[label]:
         if isinstance(value, (list, tuple)):
             if self._new_value[label][index] in value:
@@ -113,6 +127,7 @@ def _is_new_var(self, label: str, index: int, value: Any) -> bool:
         
         return True  
         
+    # The origina value to be set
     if isinstance(value, list):
         if self._org_value[label][index] == value:
             return False
@@ -124,16 +139,68 @@ def _is_new_var(self, label: str, index: int, value: Any) -> bool:
     
     return True
 
-def _get_target_frame(self, target_name: str) -> None:
+# Will update list type annotation later
+
+def _find_match_var(self, label: str, index: int) -> None:
+    # Used to retrieve the original value for `revert()
+
+    # var_ref is a list or tuple
+    var_ref = self._var_list[label][index]
+
+    # `value` is a list
+    for key, value in self._var_list.items(): 
+        # `list_val` is a list or tuple or None
+        for i, list_val in enumerate(value):
+            if list_val is None:
+                continue
+            elif isinstance(list_val, tuple) and isinstance(var_ref, tuple):
+                if self._is_ref_match(list_val, var_ref):
+                    self._org_value[label][index] = self._org_value[key][i]                 
+            elif isinstance(list_val, tuple) and isinstance(var_ref, list):
+                for i_2, ref_v in enumerate(var_ref):
+                    if self._is_ref_match(list_val, ref_v):
+                        self._org_value[label][index][i_2] = (
+                            self._org_value[key][i]
+                        )
+            elif isinstance(list_val, list) and isinstance(var_ref, tuple):
+                for i_2, l_v in enumerate(list_val):
+                    if self._is_ref_match(l_v, var_ref):
+                        self._org_value[label][index] = (
+                            self._org_value[key][i][i_2]
+                        )
+            else:
+                for ref_i, ref_val in enumerate(var_ref):
+                    for list_i, v in enumerate(list_val):
+                        if self._is_ref_match(v, ref_val):
+                            self._org_value[label][index][ref_i] = (
+                                self._org_value[key][i][list_i]
+                            )
+
+def _is_ref_match(
+        self,
+        list_val: tuple[str, ...], target_val: tuple[str, ...],
+) -> bool:
+    if len(list_val) == 2 and len(target_val) == 2:
+        return list_val[1] == target_val[1]
+
+    elif len(list_val) == 3 and len(target_val) == 3:
+        return list_val[1:] == target_val[1:]
+
+def _get_target_frame(self, target_name: str | list[str, str]) -> None:
    if self._frame is not None:
       return
    
    frame = inspect.currentframe()
 
    while frame:
-      if frame.f_code.co_name == target_name:
+      if isinstance(target_name, list):
+          if frame.f_code.co_name in target_name:
+              self._frame = frame.f_back
+              return
+      elif frame.f_code.co_name == target_name:
          self._frame = frame.f_back
          return     
+      
       frame = frame.f_back
 
 def _clear_frame(self) -> None:
