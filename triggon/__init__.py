8cd197a04e36c2b2b40745d24ad428c2c3a941cb
Tsuruko <tsuruko-12@outlook.com>
2025-07-17 10:23:30 +0900

    chore: bump version

--- triggon/__init__.py
+++ triggon/__init__.py
@@ -1,8 +1,15 @@
 from .triggon import Triggon
 from .trig_func import TrigFunc
-from ._exceptions import InvalidArgumentError
+from ._internal._exceptions import (
+    InvalidArgumentError, 
+    MissingLabelError, 
+)
 
-__version__ = "0.1.0b2"
+__version__ = "0.1.0b3"
 
-__all__ = ["Triggon", "TrigFunc", "InvalidArgumentError"]
+__all__ = [
+    "Triggon", "TrigFunc", 
+    "InvalidArgumentError", 
+    "MissingLabelError",
+]
 
