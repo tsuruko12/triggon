798d05475b7552223f244822d83a80bc7819a8e7
tsuruko <tsuruko-12@outlook.com>
2025-07-17 10:41:09 +0900

    refactor: コード整理

--- triggon/_var_update.py
+++ triggon/_var_update.py
@@ -1,19 +1,31 @@
 import inspect
 from typing import Any
 
-from ._exceptions import SYMBOL
+from ._exceptions import (
+    _UnsetExitError, 
+    SYMBOL,
+)
+from ._switch_var import _is_mult_vars
 
 
 def _store_org_value(
     self, label: str, index: int, org_value: Any,
 ) -> None:
-    if isinstance(org_value, (list, tuple)):
-      self._org_value[label][index] = []  
+    target_index = self._org_value[label][index] 
 
+    if target_index is None:
+        self._org_value[label][index] = org_value
+        return
+    elif not isinstance(target_index, list):
+        self._org_value[label][index] = [target_index]
+    elif not _is_mult_vars(target_index):
+        self._org_value[label][index] = [target_index]
+
+    if isinstance(org_value, (list, tuple)):
       for v in org_value:
           self._org_value[label][index].append(v)
     else:
-      self._org_value[label][index] = org_value
+      self._org_value[label][index].append(org_value)
 
 def _update_var_value(
     self, 
@@ -29,12 +41,14 @@ def _update_var_value(
         for value in var_ref:
             if len(value) == 3:
                 cur_value = var_ref[2].__dict__[var_ref[1]]
+
                 if cur_value == update_value:
                     continue
 
                 setattr(value[2], value[1], update_value)
             else:
                 cur_value = self._frame.f_globals[value[1]]
+
                 if cur_value == update_value:
                     continue 
 
@@ -42,24 +56,22 @@ def _update_var_value(
     else:     
         if len(var_ref) == 3:
             cur_value = var_ref[2].__dict__[var_ref[1]]
+
             if cur_value == update_value:
                 return
             
             setattr(var_ref[2], var_ref[1], update_value)
         else:  
             cur_value = self._frame.f_globals[var_ref[1]]
+
             if cur_value == update_value:
                 return
             
             self._frame.f_globals[var_ref[1]] = update_value
 
-def _check_exist_label(self, label: str) -> None:
-    try:
-        self._new_value[label]
-    except KeyError:
-        raise KeyError(f"`{label}` has not been set.")
-
 def _check_label_flag(self, label: str, cond: str | None) -> None:
+    target_func = "set_trigger"
+
     name = label.lstrip(SYMBOL)
     self._check_exist_label(name)
 
@@ -67,20 +79,18 @@ def _check_label_flag(self, label: str, cond: str | None) -> None:
         return
     
     if cond is not None:
-        self._get_target_frame("set_trigger")
+        self._get_target_frame(target_func)
 
         if not self._ensure_safe_cond(cond):
             return
         
-        self._trigger_flag[name] = True
-    else:
-        self._trigger_flag[name] = True
+    self._trigger_flag[name] = True
     
-    self._label_has_var(name, "set_trigger", False)
+    self._label_has_var(name, target_func, False)
 
     if self.debug:
-        self._get_target_frame("set_trigger")
-        self._print_flag_debug(name, "active", reset=False)
+        self._get_target_frame(target_func)
+        self._print_flag_debug(name, "active", clear_frame=False)
 
 def _label_has_var(
     self, label: str, called_func: str, to_org: bool,
@@ -98,107 +108,47 @@ def _label_has_var(
     # 特定ラベルの全ての登録されてる変数を、
     # 元の値または、設定された値に更新する
     for i in range(len(self._var_list[label])):
-        arg = self._var_list[label][i]
+        var_ref = self._var_list[label][i]
 
-        if arg is None:
+        if var_ref is None:
             continue
-        elif isinstance(arg, list):
-            for i_2, v in enumerate(arg):
+        elif isinstance(var_ref, list):
+            for i_2, val in enumerate(var_ref):
                 if not isinstance(update_value[i], (list, tuple)):
-                    self._update_var_value(v, update_value[i])
+                    self._update_var_value(val, update_value[i])
                     continue            
 
-                self._update_var_value(v, update_value[i][i_2])
+                self._update_var_value(val, update_value[i][i_2])
         else:
-            self._update_var_value(arg, update_value[i])  
+            self._update_var_value(var_ref, update_value[i])    
 
-def _is_new_var(self, label: str, index: int, value: Any) -> bool:
-    if self._trigger_flag[label]:
-        if isinstance(value, (list, tuple)):
-            if self._new_value[label][index] in value:
-                return False
-        else:  
-            self._new_value[label][index] == value
-            return False
-        
-        return True  
-        
-    if isinstance(value, list):
-        if self._org_value[label][index] == value:
-            return False
-    elif isinstance(value, tuple):
-        if tuple(self._org_value[label][index]) == value:
-            return False
-    elif self._org_value[label][index] == value:
-            return False
-    
-    return True
-
-def _find_match_var(self, label: str, index: int) -> None:
-    # `revert()`で変数の変更前の値を探す
-
-    # `var_ref` は リスト
-    var_ref = self._var_list[label][index]
-    stop_flag = False
-
-    # `value` はリスト
-    for key, value in self._var_list.items(): 
-        # `list_val` はリスト、タプル、Noneのいずれか
-        for i, list_val in enumerate(value):
-            if stop_flag:
-                break
-
-            if key == label:
-                stop_flag = True
-            elif list_val is None:
-                continue
-            elif isinstance(list_val, tuple) and isinstance(var_ref, tuple):
-                if self._is_ref_match(list_val, var_ref):
-                    self._org_value[label][index] = self._org_value[key][i]                 
-            elif isinstance(list_val, tuple) and isinstance(var_ref, list):
-                for i_2, ref_v in enumerate(var_ref):
-                    if self._is_ref_match(list_val, ref_v):
-                        self._org_value[label][index][i_2] = (
-                            self._org_value[key][i]
-                        )
-            elif isinstance(list_val, list) and isinstance(var_ref, tuple):
-                for i_2, l_v in enumerate(list_val):
-                    if self._is_ref_match(l_v, var_ref):
-                        self._org_value[label][index] = (
-                            self._org_value[key][i][i_2]
-                        )
-            else:
-                for ref_i, ref_val in enumerate(var_ref):
-                    for list_i, v in enumerate(list_val):
-                        if self._is_ref_match(v, ref_val):                           
-                            self._org_value[label][index][ref_i] = (
-                                self._org_value[key][i][list_i]
-                            )
-
-def _is_ref_match(
-        self,
-        list_val: tuple[str, ...], target_val: tuple[str, ...],
-) -> bool:
-    if len(list_val) == 2 and len(target_val) == 2:
-        return list_val[1] == target_val[1]
-
-    elif len(list_val) == 3 and len(target_val) == 3:
-        return list_val[1:] == target_val[1:]
-
-def _get_target_frame(self, target_name: str) -> None:
+def _get_target_frame(
+        self, target_name: str | list[str, str], has_exit: bool=False,
+) -> None:
    if self._frame is not None:
       return
-   
+
    frame = inspect.currentframe()
 
    while frame:
-      if frame.f_code.co_name == target_name:
+      if has_exit:
+          if frame.f_code.co_name == "<module>":
+              raise _UnsetExitError()
+          elif frame.f_code.co_name == target_name:
+              return
+      elif isinstance(target_name, list):
+          if frame.f_code.co_name in target_name:
+              self._frame = frame.f_back
+              return
+      elif frame.f_code.co_name == target_name:
          self._frame = frame.f_back
-         return
+         return     
+      
       frame = frame.f_back
 
 def _clear_frame(self) -> None:
    # メモリリークを防ぐために、処理後に`None`に設定する
    self._frame = None
    self._lineno = None
+
 
