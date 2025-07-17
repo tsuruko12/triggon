472cde9845dcc7f09de43dc040d8afb48665ab43
Tsuruko <tsuruko-12@outlook.com>
2025-07-17 10:34:51 +0900

    types: update type stubs

---
+++ triggon/typing/triggon.pyi
@@ -0,0 +1,47 @@
+from . import TrigFunc
+from typing import Any
+
+
+class Triggon:
+    def __init__(
+        self, label: str | dict[str, Any], /, new: Any=None, 
+        *, debug: bool=False,
+    ) -> None: ...
+
+    def set_trigger(
+        self, label: str | list[str] | tuple[str, ...], /, *, cond: str=None,
+    ) -> None: ...
+
+    def alter_literal(
+        self, label: str | list[str] | tuple[str, ...], /, org: Any, 
+        *, index: int=None,
+    ) -> Any: ...
+
+    def switch_lit(
+        self, label: str | list[str] | tuple[str, ...], /, org: Any, 
+        *, index: int=None,
+    ) -> Any: ...
+
+    def alter_var(
+          self, label: str | dict[str, Any], var: Any=None, /, 
+          *, index: int=None,
+    ) -> None | Any: ...
+
+    def switch_var(
+          self, label: str | dict[str, Any], var: Any=None, /, 
+          *, index: int=None,
+    ) -> None | Any: ...
+
+    def revert(
+            self, label: str | list[str] | tuple[str, ...], /, 
+            *, all: bool=False, disable: bool=False,
+    ) -> None: ...
+
+    def exit_point(self, label: str, func: TrigFunc, /) -> None | Any: ...    
+
+    def trigger_return(
+        self, label: str, /, ret: Any=None, 
+        *, index: int=None, do_print: bool=False,
+    ) -> None | Any: ...
+
+    def trigger_func(self, label: str, func: TrigFunc, /) -> None | Any: ...
