0f33b44c964569348187786627ddf03ee55b3a56
Tsuruko <tsuruko-12@outlook.com>
2025-07-22 04:13:48 +0900

    chore: bump version to v0.1.b4

--- triggon/__init__.py
+++ triggon/__init__.py
@@ -3,15 +3,13 @@ from .trig_func import TrigFunc
 from ._internal._exceptions import (
     InvalidArgumentError, 
     MissingLabelError, 
-    VariableNotFoundError,
 )
 
-__version__ = "0.1.0b3"
+__version__ = "0.1.0b4"
 
 __all__ = [
     "Triggon", "TrigFunc", 
     "InvalidArgumentError", 
     "MissingLabelError",
-    "VariableNotFoundError",
 ]
 
