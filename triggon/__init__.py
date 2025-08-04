77c791d9d9eceedda99ba1bdfadb2f1c9d6c6780
Tsuruko <tsuruko-12@outlook.com>
2025-08-05 00:17:31 +0900

    style: reorder

--- triggon/__init__.py
+++ triggon/__init__.py
@@ -1,15 +1,16 @@
-from .triggon import Triggon
-from .trig_func import TrigFunc
-from ._internal._exceptions import (
-    InvalidArgumentError, 
-    MissingLabelError, 
-)
-
 __version__ = "0.1.0b4"
 
 __all__ = [
-    "Triggon", "TrigFunc", 
-    "InvalidArgumentError", 
+    "Triggon", 
+    "TrigFunc",
+    "InvalidArgumentError",
     "MissingLabelError",
 ]
+
+from .triggon import Triggon
+from .trig_func import TrigFunc
+from ._internal._exceptions import (
+    InvalidArgumentError,
+    MissingLabelError,
+)
 
