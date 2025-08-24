640d9e49a642c548de18a25339bad5b3de2116ab
Tsuruko <tsuruko-12@outlook.com>
2025-08-25 06:17:07 +0900

    feat: support for library functions and add debug trace

--- triggon/trig_func.py
+++ triggon/trig_func.py
@@ -1,40 +1,66 @@
+import builtins
 import inspect
 from typing import Any, Callable
+
+from ._internal._exceptions import InvalidArgumentError
+from ._internal._sentinel import _NO_VALUE
 
 
 class TrigFunc:
   """
-  Delays function execution for use with `trigger_return()` or `trigger_func()`.
+  Call a function without executing it immediately.
 
-  Use this wrapper to pass functions without calling them.
+  For better performance, create and assign an instance first 
+  before calling the target function.
 
-  You must assign the instance (e.g., F = TrigFunc()) 
-  before calling the trigger function.
+  To work correctly in functions of the Triggon class, always add '()' 
+  when calling the target function (e.g., F = TrigFunc(), F.test()).
   """
 
-  _func: Callable | None
+  _func: Any | None
 
-  def __init__(self, func: Callable = None, /) -> None:
-    self._func = func
+  def __init__(
+      self, _func: Any | None = _NO_VALUE, _chain: bool = False,
+  ) -> None:
+    if _func is not _NO_VALUE and not _chain:
+      raise InvalidArgumentError("Arguments cannot be passed.")
+    
+    self._func = _func
 
-  def __call__(self, *args, **kwargs) -> "TrigFunc":
-    if self._func is None:
-      raise ValueError("`func` is None")
+  def __call__(self, *args: Any, **kwargs: Any) -> "TrigFunc":
+    if self._func is _NO_VALUE:
+      raise TypeError("No function was provided.")
+    return TrigFunc(self._func(*args, **kwargs), _chain=True)
     
-    return TrigFunc(self._func(*args, **kwargs))
-    
-  def __getattr__(self, name: str) -> Callable[[], Any]:    
-    if self._func is not None:
-      target = getattr(self._func, name)
+  def __getattr__(self, name: str) -> Callable[..., Any] | "TrigFunc":  
+    if self._func is not _NO_VALUE:
+      target = getattr(self._func, name, None)
+      if target is None:
+        target = getattr(builtins, name, None)
     else:
       frame = inspect.currentframe().f_back
-      target = frame.f_locals.get(name) or frame.f_globals.get(name)
+      target = frame.f_locals.get(name)
+
+      if target is None:
+        target = frame.f_globals.get(name)
+        if target is None:
+          target = getattr(builtins, name, None)
+
+      frame = None
 
     if target is None:
-        raise AttributeError(f"'{name}' is not a callable function")
-    elif not callable(target):
-        return TrigFunc(target)
+        raise AttributeError(f"'{name}' is not a callable function.")
+    if not callable(target):
+        return TrigFunc(target, _chain=True)
       
-    def _wrapper(*args, **kwargs) -> Callable[[], Any]:
-      return lambda: target(*args, **kwargs)
+    def _wrapper(*args: Any, **kwargs: Any) -> Callable[..., Any]:
+      func = lambda: target(*args, **kwargs)
+      func._trigfunc = True # For checking if this class is used.
+      
+      # For debug
+      func._trigfunc_name = name
+      func._trigfunc_args = args
+      func._trigfunc_kwargs = kwargs
+
+      return func
     return _wrapper
