6f3d2f297aaffdbbbca6ed67f207a2b5dbfcfde6
Tsuruko <tsuruko-12@outlook.com>
2025-08-25 06:25:26 +0900

    fix: adjust internal function behavior to resolve bug

--- triggon/_internal/_switch_var.py
+++ triggon/_internal/_switch_var.py
@@ -1,8 +1,13 @@
-def _init_or_not(self, label: str, index: int | tuple[int, ...]) -> bool:
-    self._get_target_frame(["switch_var", "alter_var"]) # Will change it after beta 
+from typing import Any
 
-    self._check_exist_label(label)  
-    self._compare_value_counts(label, index, allow_tuple=True)
+from ._sentinel import _NO_VALUE
+
+
+def _init_or_not(self, label: str, index: tuple[int, ...]) -> bool:
+    self._get_target_frame("switch_var")
+
+    self._ensure_label_exists(label)  
+    self._compare_value_counts(label, index)
 
     if (
         self._var_refs[label][index[0]] is None 
@@ -10,87 +15,68 @@ def _init_or_not(self, label: str, index: int | tuple[int, ...]) -> bool:
     ):  
         # Initial process to store argument variables 
         self._trace_func_call()
-        return True
-    
+        return True 
     return False
 
 def _is_new_var(
-        self, label: str, index: int, target_ref: tuple[str, ...] = None,
+        self, label: str, index: int, target_refs: tuple[str, ...] = None,
 ) -> bool:
-    # If both the file name and line number match,
-    # the variable is already registered.
+    var_refs = self._var_refs[label][index]
 
-    var_ref = self._var_refs[label][index]
+    if var_refs is None:
+        return 
+    if isinstance(var_refs, tuple):
+        var_refs = [var_refs]
 
-    if isinstance(var_ref, tuple):
-        if target_ref is not None:
-            if target_ref[:3] == var_ref[:3]:
-                return False
-            else:
-                return True
-        elif var_ref[0] == self._file_name and var_ref[1] == self._lineno:
+    if target_refs is not None:
+        if len(target_refs) == 3:
+            cmp_tar_refs = (target_refs[0], target_refs[2])
+        elif len(target_refs) == 4:
+            cmp_tar_refs = (target_refs[2], target_refs[3])
+    else:
+        cmp_tar_refs = (self._file_name, self._lineno)
+
+    for ref in var_refs:
+        if target_refs is not None:
+            if len(ref) == 3:
+                # (file name, var name)
+                cmp_refs = (ref[0], ref[2])
+            elif len(ref) == 4:
+                # (attr name, class instance)
+                cmp_refs = (ref[2], ref[3])
+        else:
+            # (file name, line number)
+            cmp_refs = (ref[0], ref[1])
+
+        if cmp_refs == cmp_tar_refs:
             return False
-        
-        return True
-    
-    for ref in var_ref:
-        if target_ref is not None:
-            if target_ref[:3] == var_ref[:3]:
-                return False
-        elif ref[0] == self._file_name and ref[1] == self._lineno:
-            return False
-        
+             
     return True
 
-def _find_match_var(self, label: str = None, index: int = None) -> None:
-    # Used to retrieve the original value for `revert()`
+def _find_match_var(self, label: str, target_refs: tuple[str, ...]) -> Any:
+    # Used to retrieve the original value for revert()
 
-    # it can be a list or tuple
-    var_ref = self._var_refs[label][index]
-
-    # Searrch up to the same label
-    stop_flag = False
-
-    # `value` is a list
+    # value is a list
     for key, value in self._var_refs.items(): 
-        if stop_flag:
-            return
-        
-        if key == label:
-            stop_flag = True
-
-        # `list_val` is a list or tuple or None
+        # list_val is a list or tuple or None
         for i, list_val in enumerate(value):
             if list_val is None:
                 continue
-            elif isinstance(list_val, tuple) and isinstance(var_ref, tuple):
-                if self._is_ref_match(list_val, var_ref):
-                    self._org_values[label][index] = self._org_values[key][i]     
-            elif isinstance(list_val, tuple) and isinstance(var_ref, list):
-                for i_2, ref_v in enumerate(var_ref):
-                    if self._is_ref_match(list_val, ref_v):
-                        self._org_values[label][index][i_2] = (
-                            self._org_values[key][i]
-                        )
-            elif isinstance(list_val, list) and isinstance(var_ref, tuple):
-                for i_2, l_v in enumerate(list_val):
-                    if self._is_ref_match(l_v, var_ref):
-                        self._org_values[label][index] = (
-                            self._org_values[key][i][i_2]
-                        )
+            elif isinstance(list_val, tuple):
+                if self._is_ref_match(list_val, target_refs):
+                    return self._org_values[key][i][0]  
             else:
-                for ref_i, ref_val in enumerate(var_ref):
-                    for list_i, v in enumerate(list_val):
-                        if self._is_ref_match(v, ref_val):                           
-                            self._org_values[label][index][ref_i] = (
-                                self._org_values[key][i][list_i]
-                            )
+                for i_2, val in enumerate(list_val):
+                    if self._is_ref_match(val, target_refs):
+                        return self._org_values[key][i][i_2]
+                    
+    return _NO_VALUE
 
 def _is_ref_match(
         self, list_val: tuple[str, ...], target_val: tuple[str, ...],
 ) -> bool:
     if len(list_val) == 3 and len(target_val) == 3:
         return list_val[0] == target_val[0] and list_val[2] == target_val[2]
-    elif len(list_val) == 4 and len(target_val) == 4:
+    if len(list_val) == 4 and len(target_val) == 4:
         return list_val[2:] == target_val[2:]
     
