1ab17021f94221f99909c2a2fd33a74193544928
Tsuruko <tsuruko-12@outlook.com>
2026-02-24 17:19:28 +0900

    style: format code

--- triggon/core/value_update.py
+++ triggon/core/value_update.py
@@ -12,13 +12,13 @@ from .._internal.arg_types import (
 
 class ValueUpdater:
     def update_values(
-            self, 
-            label: str, 
-            idx: int | None, 
-            f_globals: dict[str, Any],
-            callsite: Callsite,
-            set_true: bool,
-    ) -> None:  
+        self,
+        label: str,
+        idx: int | None,
+        f_globals: dict[str, Any],
+        callsite: Callsite,
+        set_true: bool,
+    ) -> None:
         debug_on = self.debug[TRIGGON_LOG_VERBOSITY] > 1
         label_value = self._new_values[label]
 
@@ -31,43 +31,43 @@ class ValueUpdater:
             ref_id, var_name = ref
 
             new_value, label_idx = self._get_new_value_and_idx(
-                set_true, 
-                label_value, 
-                idx, 
+                set_true,
+                label_value,
+                idx,
                 ref_id,
-            ) 
-            with UPDATE_LOCK:     
+            )
+            with UPDATE_LOCK:
                 try:
                     prev_value = f_globals[var_name]
                     if prev_value == new_value:
                         continue
                     if set_true and hasattr(new_value, TRIGFUNC_ATTR):
-                        f_globals[var_name] = new_value.run() 
+                        f_globals[var_name] = new_value.run()
                     else:
-                        f_globals[var_name] = new_value   
+                        f_globals[var_name] = new_value
                 except KeyError as e:
                     raise UpdateError(var_name, e) from None
                 else:
                     if debug_on:
                         self.log_value_update(
-                            label, 
-                            label_idx,  
+                            label,
+                            label_idx,
                             prev_value,
-                            new_value, 
+                            new_value,
                             callsite,
                             target_name=var_name,
-                        ) 
+                        )
 
         # Update attributes
         for ref in attr_refs:
             ref_id, attr_name, obj, full_name = ref
 
             new_value, label_idx = self._get_new_value_and_idx(
-                set_true, 
-                label_value, 
-                idx, 
+                set_true,
+                label_value,
+                idx,
                 ref_id,
-            )   
+            )
             with UPDATE_LOCK:
                 try:
                     prev_value = getattr(obj, attr_name)
@@ -82,22 +82,22 @@ class ValueUpdater:
                 else:
                     if debug_on:
                         self.log_value_update(
-                            label, 
-                            label_idx,  
+                            label,
+                            label_idx,
                             prev_value,
-                            new_value, 
+                            new_value,
                             callsite,
                             target_name=full_name,
-                        ) 
+                        )
 
     def _get_new_value_and_idx(
-            self, 
-            set_true: bool, 
-            label_value: tuple[Any, ...],
-            idx: int | None,
-            ref_id: int, 
+        self,
+        set_true: bool,
+        label_value: tuple[Any, ...],
+        idx: int | None,
+        ref_id: int,
     ) -> tuple[Any, int | None]:
-        if not set_true:                 
+        if not set_true:
             new_value = self._id_meta[ref_id][1]
         else:
             if idx is None:
@@ -111,7 +111,9 @@ class ValueUpdater:
         return idxs[-1]
 
     def _find_update_refs(
-            self, label: str, file_name: str,
+        self,
+        label: str,
+        file_name: str,
     ) -> tuple[list[VarRef], list[AttrRef]]:
         label_refs = self._label_refs[label]
         var_refs = label_refs["var"]
@@ -122,10 +124,9 @@ class ValueUpdater:
         ]
         target_attr_refs = [
             ref for ref in attr_refs if self._is_from_file(ref, file_name)
-        ]     
+        ]
         return target_var_refs, target_attr_refs
-    
+
     def _is_from_file(self, ref: VarRef | AttrRef, file_name: str) -> bool:
         ref_id = ref[0]
         return self._id_meta[ref_id][0] != file_name
-        
