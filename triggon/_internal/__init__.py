95f06f5612c6908712f9cd0bc08e7ce881aec2f9
Tsuruko <tsuruko-12@outlook.com>
2026-02-28 17:08:39 +0900

    chore: add __init__.py

---
+++ triggon/_internal/__init__.py
@@ -0,0 +1,18 @@
+from .keys import ATTR, VAR
+from .lock import UPDATE_LOCK
+from .log_setup import LOG_VERBOSITY, logger
+from .mixins import _Internal
+from .sentinel import _NO_VALUE
+from .type_checks import check_arg_values
+from .utils import to_dict
+
+
+all = [
+    "LOG_VERBOSITY",
+    "UPDATE_LOCK",
+    "_Internal",
+    "_NO_VALUE",
+    "check_arg_values",
+    "logger",
+    "to_dict",
+]
