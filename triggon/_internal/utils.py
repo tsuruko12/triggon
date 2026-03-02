5fd493437f549920dfc1122024c831401d4744d2
Tsuruko <tsuruko-12@outlook.com>
2026-03-02 18:02:30 +0900

    chore: add utils.py

--- triggon/_internal/utils.py
+++ triggon/_internal/utils.py
@@ -1,11 +1,12 @@
 def to_dict(
-        keys: tuple[str, ...], values: tuple[str | int, ...] | None,
+    keys: tuple[str, ...],
+    values: tuple[str | int, ...] | None,
 ) -> dict[str, str | int | None]:
     if values is None:
         values_iter = (None,) * len(keys)
     else:
         values_iter = values
-        
+
     label_values = {}
     for key, val in zip(keys, values_iter):
         label_values[key] = val
