1c5c27e0f09f844e2461e0497f523969cda90ffa
Tsuruko <tsuruko-12@outlook.com>
2026-03-03 21:09:38 +0900

    refactor: add key constants

--- triggon/_internal/keys.py
+++ triggon/_internal/keys.py
@@ -1,2 +1,9 @@
 ATTR = "attr"
 VAR = "var"
+
+TRIGGER = "trigger"
+REVERT = "revert"
+
+LOG_VERBOSITY = "TRIGGON_LOG_VERBOSITY"  # 0-3
+LOG_FILE = "TRIGGON_LOG_FILE"
+LOG_LABELS = "TRIGGON_LOG_LABELS"  # target labels to output
