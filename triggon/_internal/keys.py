50ccd335b7f24e5a8ebaf4d2d76e0e25c4b56409
Tsuruko <tsuruko-12@outlook.com>
2026-03-07 21:53:12 +0900

    style: add constant comments

--- triggon/_internal/keys.py
+++ triggon/_internal/keys.py
@@ -1,10 +1,12 @@
+# Reference Keys
 ATTR = "attr"
-GLOB_VAR = "glob_var"
-LOC_VAR = "local_var"
+VAR = "var"
 
+# Delay Keys
 TRIGGER = "trigger"
 REVERT = "revert"
 
+# Debug Keys
 LOG_VERBOSITY = "TRIGGON_LOG_VERBOSITY"  # 0-3
 LOG_FILE = "TRIGGON_LOG_FILE"
 LOG_LABELS = "TRIGGON_LOG_LABELS"  # target labels to output
