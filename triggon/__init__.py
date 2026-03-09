9402c5f61f79f58040b0c46705fd4a5b95c57548
Tsuruko <tsuruko-12@outlook.com>
2026-03-09 16:09:37 +0900

    refactor: update imports and version in __init__.py

--- triggon/__init__.py
+++ triggon/__init__.py
@@ -1,19 +1,21 @@
-from .triggon import Triggon
-from .trig_func import TrigFunc
-from ._internal._exceptions import (
+from .api import Triggon
+from .errors.public import (
+    FrameAccessError,
     InvalidArgumentError,
-    InvalidClassVarError,
-    MissingLabelError,
+    RollbackNotSupportedError,
+    UnregisteredLabelError,
+    UpdateError,
 )
+from .trigfunc import TrigFunc
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
 __all__ = [
     "Triggon",
     "TrigFunc",
+    "FrameAccessError",
     "InvalidArgumentError",
-    "InvalidClassVarError",
-    "MissingLabelError",
+    "RollbackNotSupportedError",
+    "UnregisteredLabelError",
+    "UpdateError",
 ]
-
-
