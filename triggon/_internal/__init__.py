c8ce4903d8d271de85fe16c0aa4723ed9d440d00
Tsuruko <tsuruko-12@outlook.com>
2026-03-10 21:58:08 +0900

    refactor: update logger import path to debug.setup

--- triggon/_internal/__init__.py
+++ triggon/_internal/__init__.py
@@ -1,4 +1,4 @@
-from .log_setup import logger
+from .debug.setup import logger
 from .mixins import _Internal
 from .rollback_ast import collect_rollback_refs, revert_targets
 from .utils import to_dict, unwrap_value
