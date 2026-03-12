beee4b9c23415a20575515184a61f6ee340181fc
Tsuruko <tsuruko-12@outlook.com>
2026-03-12 16:33:53 +0900

    feat: add MODULE_SCOPE key

--- triggon/_internal/keys.py
+++ triggon/_internal/keys.py
@@ -11,3 +11,6 @@ REVERT = "revert"
 LOG_VERBOSITY = "TRIGGON_LOG_VERBOSITY"  # 0-3
 LOG_FILE = "TRIGGON_LOG_FILE"
 LOG_LABELS = "TRIGGON_LOG_LABELS"  # target labels to output
+
+# Scope Keys
+MODULE_SCOPE = "module"
