3696d6d4e0e114c76ddd03049a78e0d978f69393
Tsuruko <tsuruko-12@outlook.com>
2026-03-14 21:06:43 +0900

    chore: remove old top-level triggon package directory

--- triggon/_internal/mixins.py
+++
@@ -1,7 +0,0 @@
-from .debug.logger import DebugLogger
-from .label import LabelValidator
-from .debug.setup import LogSetup
-
-
-class _Internal(LabelValidator, LogSetup, DebugLogger):
-    """Internal mixin bundle."""
--- triggon/trigfunc/__init__.py
+++
@@ -1,3 +0,0 @@
-from .trigfunc import TRIGFUNC_ATTR, TrigFunc
-
-__all__ = ["TrigFunc", "TRIGFUNC_ATTR"]
